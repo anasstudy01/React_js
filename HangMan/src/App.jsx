70bc56f8af8167987ae1d52970c857f833157535
anas <anasstudy01@gmail.com>
2025-03-04 04:22:12 +0530

    Add initial React components and Tailwind CSS integration

---
+++ HangMan/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App(){
+  return (
+    <div>
+      <h1 className="font-semibold text-4xl">Hello react developers</h1>
+    </div>
+  )
+}
+ export default App;
---
+++ reactStarterProject/src/CustomComponet.jsx
@@ -0,0 +1,6 @@
+function CustomComponent(){
+    return <div>THIS IS A DIV WHICH HELPS TO REPEAT THE TASK </div>;
+    
+}
+
+export default CustomComponent;
