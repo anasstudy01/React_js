0927be7e8a6ee3b5da5fb437578113941177df8b
anas <anasstudy01@gmail.com>
2025-03-10 12:53:25 +0530

    Implement routing with react-router-dom and add PlayGame and StartGame components

--- HangMan/src/App.jsx
+++ HangMan/src/App.jsx
@@ -1,18 +1,24 @@
-import  Button  from "./Components/Button/Button";
-import TextInput from "./Components/TextInput/TextInput";
-import TextInputForm from  "./Components/TextInput/TextInputForm";
+
+import { Route, Routes } from "react-router-dom";
 import TextInputFormContainer from "./Components/TextInput/TextInputFormContainer";
+import PlayGame from "./Components/Pages/PlayGame/PlayGame";
+import StartGame from "./Components/Pages/StartGame/StartGame";
 
 function App() {
   return (
-    <div>
-      <center>
-        <h1 className="text-3xl bg-yellow-100 border-2 border-gray-500 ">
-          Welcome to Hangman
-        </h1>
-      </center>
-      <TextInputFormContainer  onSubmit={(value)=>{console.log(`the submiteed value is ${value}`)}}/>
-    </div>
+    // <div>
+    //   <center>
+    //     <h1 className="text-3xl bg-yellow-100 border-2 border-gray-500 ">
+    //       Welcome to Hangman
+    //     </h1>
+    //   </center>
+    //   <TextInputFormContainer  onSubmit={(value)=>{console.log(`the submiteed value is ${value}`)}}/>
+    // </div>
+    <Routes>
+<Route path="/play" element={<PlayGame />} />
+<Route path="/start" element={<StartGame/>} />
+<Route path="*" element={<div>not found page</div>} />
+    </Routes>
   );
 }
 export default App;
---
+++ HangMan/src/Components/Pages/StartGame/StartGame.jsx
@@ -0,0 +1,11 @@
+function StartGame(){
+
+return(<div>
+    <h1>Start Game</h1>
+    <p>Click on the button to start the game</p>
+   
+</div>);
+
+}
+
+export default StartGame;
