7414bd0c2c4010d0ca965bd860de6803fd25cc3a
anas <anasstudy01@gmail.com>
2025-03-22 04:05:24 +0530

    Replace banner image format and adjust styling in Banner component

--- 08CoinGeckoCryptoTracker/src/componets/banner/Banner.jsx
+++ 08CoinGeckoCryptoTracker/src/componets/banner/Banner.jsx
@@ -1,4 +1,4 @@
-import BannerImage from '../banner/Banner1.PNG';
+import BannerImage from '../banner/banner1.jpeg';
 
 
 function Banner() {
@@ -9,11 +9,11 @@ function Banner() {
 <div className='w-full h-[25rem] relative'>
     <img src={BannerImage} alt="Banner" className="w-full px-8 h-96"  />
 
-    <div className='absolute top-40 left-200 right-0 mx-auto w-[20rem] text-white flex flex-col items-center'>
-   <div className=' font-semibold txt-xl text-white font-sans'>
+    <div className='absolute top-40 left-0 right-200 mx-auto w-140 text-white flex flex-col items-center gap-1'>
+   <div className=' font-semibold text-5xl text-white ml-10 font-sans'>
    Invest Like a Pro
    </div>
-   <div className='text-sm text-white ml-11'>
+   <div className='text-sm text-white ml-0'>
     Get the latest updates on crypto prices and trends  
 
 
