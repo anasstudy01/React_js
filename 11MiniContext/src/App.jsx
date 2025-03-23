c3ee1fc2b249df71466f4854813f6d367be63f11
anas <anasstudy01@gmail.com>
2025-03-23 21:34:51 +0530

    Refactor layout in App component for improved structure and styling

--- 11MiniContext/src/App.jsx
+++ 11MiniContext/src/App.jsx
@@ -10,16 +10,16 @@ function App() {
   return (
     <>
     <Valuelelo.Provider value={{count,setCount}}>
-      <div className="w-full bg-black h-screen text-white flex justify-center  items-center">
+      <div className="w-full bg-black h-screen text-white flex justify-center flex-col items-center">
         <div className="relative bg-gray-800 w-200 h-100 p-4 rounded-xl text-center">
         
           <h1 className=" text-3xl border-3 rounded-xl p-2 px-4 ">
             welcome to react prop Driling projects
           </h1>
 <button onClick={()=>setCount(count+1)} className="border-3 px-4 py-2 my-5 rounded-xl">Counter    {count} </button>
- <Navbar/>
 
         </div>
+ <Navbar/>
       </div>
 
     </Valuelelo.Provider>
