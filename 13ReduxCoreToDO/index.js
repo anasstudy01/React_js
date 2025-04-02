3203a314dcba58269293444cead8f13572428733
anas <anasstudy01@gmail.com>
2025-04-02 08:18:57 +0530

    Add initial todo actions and export store from Redux setup

--- 13ReduxCoreToDO/index.js
+++ 13ReduxCoreToDO/index.js
@@ -1,1 +1,5 @@
- 
+ import store from "./store.js";
+store.dispatch({  type: "add",payload: "Learn Redux",});
+store.dispatch({ type: "add", payload: "Learn React" });
+store.dispatch({ type: "add", payload: "Learn JavaScript" });
+store.dispatch({ type: "remove", payload:store.getState().todos[0].id });
--- 13ReduxCoreToDO/store.js
+++ 13ReduxCoreToDO/store.js
@@ -4,12 +4,12 @@ import { add } from "./constant.js";
 
 
 
-const store = createStore(TodoReducer);
+ 
+ const  store = createStore(TodoReducer);
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({  type: "add",payload: "Learn Redux",});
-store.dispatch({ type: "add", payload: "Learn React" });
-store.dispatch({ type: "remove", payload:store.getState().todos[0].id });
+
+export default store;
