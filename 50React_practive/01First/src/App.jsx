76847e4fb6cf7a4bcb99e19917c8a6f10c4c496c
anasstudy01 <anasstudy01@gmail.com>
2025-06-18 19:40:50 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 50React_practive/01First/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Thepla from './Thepla'
+
+function App() {
+ 
+
+  return (
+    <>
+   <h1 className='text-2xl bg-black text-white border-3 border-yellow-100 text-center ' >welcome to react</h1>
+
+ <Thepla/>
+    </>
+  )
+}
+
+export default App
---
+++ 50React_practive/01First/src/Thepla.jsx
@@ -0,0 +1,11 @@
+ function Thepla(){
+    return (
+        <>
+        
+        <h3 className=" text-3xl text-blue-500 ">welcome to another chai components</h3>
+        </>
+    )
+}
+
+
+export default Thepla
